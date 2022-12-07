9f1314f21680cc7162bcb8adc45c35b961b1e4a0
Christapher Antony <christapher012@gmail.com>
2022-12-07 12:50:24 +0530

    delete added date fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         </div>
         <div className="subHeading">
           <br />
-          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+          <h2>Whoop, it's {(new Date()).toLocaleDateString()} 🌝 ☕ </h2>
         </div>
         <div className="input">
           <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
