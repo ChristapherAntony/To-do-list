de77e846a4a7c7051aa88e4ec3c99921b10fc901
Christapher Antony <christapher012@gmail.com>
2022-12-07 11:28:59 +0530

    delete added

--- src/App.js
+++ src/App.js
@@ -7,50 +7,103 @@ function App() {
   const [toDo, setToDo] = useState('')
   return (
     <div className="app">
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
-      </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
-      </div>
-      <div className="input">
-        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
-      </div>
-      <div className="todos">
-        {toDos.map((obj) => {
-          return (
-            <div className="todo">
-              <div className="left">
-                <input onChange={(e) => {
-                  console.log(e.target.checked);
-                  console.log(obj);
-                  setToDos(toDos.filter(obj2=>{
-                    if(obj2.id===obj.id){
-                      obj2.status=e.target.checked
-                    }
-                    return obj2;
-                  }))
-                }}
-                  value={obj.status} type="checkbox" name="" id="" />
-                <p>{obj.text}</p>
+      <div>
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+          <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+          {toDos.map((obj) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e) => {
+                    setToDos(toDos.filter(obj2 => {
+                      if (obj2.id === obj.id) {
+                        obj2.status = e.target.checked
+                      }
+                      return obj2;
+                    }))
+                  }}
+                    value={obj.status} type="checkbox" name="" id="" />
+                  <p>{obj.text}</p>
 
+                </div>
+
+                <div className="right">
+                  <i onClick={(e) => {
+                    setToDos(toDos.filter(obj2 => {
+                      let temp;
+                      if (obj2.id !== obj.id) {
+                        temp = obj2
+                      }
+                      return temp;
+                    }));
+                  }} className="fas fa-times"></i>
+                </div>
               </div>
-              <div className="right">
-                <i className="fas fa-times"></i>
-              </div>
-            </div>
-          )
-        })}
-        {toDos.map((obj)=>{
-          if(obj.status){
-            return( <h2>{obj.text}</h2>);
-          }
-          return null;
-           
-        })}
+            )
+          })}
+
+        </div>
+
       </div>
+      <div>
+
+        <div className="subHeading">
+          <br />
+          <h2 style={{ backgroundColor: "green" }}>Completed </h2>
+        </div>
+        <div className="todos">
+
+          {toDos.map((obj) => {
+            if (obj.status) {
+              return (
+
+                <div className="todo">
+                  <div className="left">
+                    <p>{obj.text}</p>
+                  </div>
+                  <div className="right">
+                    <i className="fas fa-check "></i>
+                  </div>
+                </div>
+              );
+            }
+            return null;
+
+          })}
+
+
+
+        </div>
+      </div>
+      <div>
+
+        <div className="subHeading">
+          <br />
+          <h2 style={{ backgroundColor: "red" }}>Cancelled </h2>
+        </div>
+        <div className="todos">
+          <div className="todo">
+            <div className="left">
+              <input type="checkbox" name="" id="" />
+              <p>Rect tutorial</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
